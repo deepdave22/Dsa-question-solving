2393c7e54af92b12115f63587fbca605d69f25b6
Deep Dave <97455623+deepdave22@users.noreply.github.com>
2024-11-20 22:18:20 +0530

    Create NextGreaterElementI.java

---
+++ easy/NextGreaterElementI.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+        int ans[] = new int[nums1.length];
+
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<nums2[i]){
+                s.pop();
+            }
+            if(s.size()>0){
+                map.put(nums2[i],s.peek());
+            }else{
+                map.put(nums2[i],-1);
+            }
+            s.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
